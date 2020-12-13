a39cb8496817bb0f7fd3365e8c3ddc7de3958512
Antti Riski <antti.riski@aalto.fi>
2020-12-13 15:32:56 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 function App() {
+  const [stations, setStations] = useState([]);
+  const [activeStation, setActiveStation] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("http://data.foli.fi/citybike");
+      setStations(result.data.racks);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Map center={[60.445, 22.264824]} zoom={13}>
+          {activeStation && (
+            <Popup
+              position={[activeStation.lat, activeStation.lon]}
+              onClose={() => {
+                setActiveStation(null);
+              }}
+            >
+              <div>
+                <h3>{activeStation.name}</h3>
+                <p>Pyöriä asemalla: {activeStation.bikes_avail}</p>
+              </div>
+            </Popup>
+          )}
+
+          {Object.values(stations).map((station) => (
+            <Marker
+              key={station.id}
+              position={[station.lat, station.lon]}
+              onclick={() => setActiveStation(station)}
+            />
+          ))}
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </Map>
+      </div>
     </div>
   );
 }
